500826ec1c6cde339bfecd734f9f437cd2a59d36
DerreckMwiti <ndambiri501@gmail.com>
2023-03-24 03:27:35 -0700

    0-sum_them_all.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,28 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
-int _putchar(char c);
 /**
-*struct dog - Initialize struct dog
-*@name:name type char
-*@age: age type float
-*@owner: name of owner tyme char
-*
-*Description: name, age and owner of a dog
-*/
-
-typedef struct dog
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* DOG_H */
+#endif
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+-m 0x10. C - Variadic functions
